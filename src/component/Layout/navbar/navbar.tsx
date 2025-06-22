7b75ce89e989d6ccdc51cd975be884b45366512e
Derek <dberb789@gmail.com>
2025-06-22 15:45:33 +0800

    feat: change to new app name and add navbutton animations

--- src/component/Layout/navbar/navbar.tsx
+++ src/component/Layout/navbar/navbar.tsx
@@ -11,22 +11,23 @@ export default function Navbar(){
         <div className="sticky top-0 w-full min-h-10 bg-background-muted shadow-md rounded-b-3xl font-serif">
             <div className="flex justify-between p-4">
                 <div className="text-foreground font-semibold text-3xl">
-                    Revisea.
+                    Stendhal.
                 </div>
-
                 <div className="flex gap-6">
-
-                {navItems && (navItems.map((item, index) => {
-                    return (
+                    {navItems && (navItems.map((item, index) => {
+                        return (
                         <div className="relative" key={index}>
                             <Link href={item.href} className="text-foreground font-light text-2xl">
                                 {item.name}
                             </Link>
-                            <span className={`absolute left-0 w-full h-[2px] rounded-full bg-foreground-muted bottom-0.5`}></span>
+                            <span
+                            className={`absolute left-0 bottom-0.5 h-[2px] rounded-full bg-foreground-muted transition-all duration-300 ${
+                                pathname === item.href ? "w-full" : "w-0"
+                            }`}
+                            />
                         </div> 
-                    )
-                }))}
-
+                        )
+                    }))}
                 </div>
             </div>
         </div>
