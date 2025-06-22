69ba63b28548e71b4481a5a436225ea105cb0f59
Derek <dberb789@gmail.com>
2025-06-22 14:54:26 +0800

    feat: create config file for navbar

---
+++ src/component/Layout/navbar/navConfig.tsx
@@ -0,0 +1,20 @@
+interface navbarItemType {
+    name: string
+    href: string
+}
+
+
+export const navItems: navbarItemType[] = [
+    {
+        name: 'Dashboard',
+        href: '/dashboard'
+    },
+    {
+        name: 'Notes',
+        href: '/notes'
+    },
+    {
+        name: 'Flashcards',
+        href: '/flashcards'
+    }
+]
