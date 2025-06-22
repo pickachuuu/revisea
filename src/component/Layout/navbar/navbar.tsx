2e872902aab26fe9d2967b481b76451c79b280b0
Derek <dberb789@gmail.com>
2025-06-22 15:26:32 +0800

    style: use serif font style

--- src/component/Layout/navbar/navbar.tsx
+++ src/component/Layout/navbar/navbar.tsx
@@ -1,22 +1,32 @@
-import { navItems } from "./navConfig"
-import Link from "next/link"
+'use client';
+
+import { navItems } from "./navConfig";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
 
 export default function Navbar(){
+    const pathname = usePathname()
+
     return (
-        <div className="sticky top-0 w-full min-h-10 bg-background-muted shadow-md rounded-b-3xl">
+        <div className="sticky top-0 w-full min-h-10 bg-background-muted shadow-md rounded-b-3xl font-serif">
             <div className="flex justify-between p-4">
-                <div className="text-foreground font-semibold text-2xl">
+                <div className="text-foreground font-semibold text-3xl">
                     Revisea.
                 </div>
 
                 <div className="flex gap-6">
+
                 {navItems && (navItems.map((item, index) => {
                     return (
-                        <Link key={index} href={item.href} className="text-foreground font-medium text-xl">
-                            {item.name}
-                        </Link> 
+                        <div className="relative" key={index}>
+                            <Link href={item.href} className="text-foreground font-light text-2xl">
+                                {item.name}
+                            </Link>
+                            <span className={`absolute left-0 w-full h-[2px] rounded-full bg-foreground-muted bottom-0.5`}></span>
+                        </div> 
                     )
                 }))}
+
                 </div>
             </div>
         </div>
