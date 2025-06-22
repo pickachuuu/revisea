34774ca7b18a42410c2c71ec74e7517fed31b7fb
Derek <dberb789@gmail.com>
2025-06-22 17:25:58 +0800

    feat: create init header component

--- src/component/ui/Header.tsx
+++ src/component/ui/Header.tsx
@@ -1,2 +1,23 @@
 import { cn } from "@/lib/utils";
 import { cva, VariantProps } from "class-variance-authority";
+
+interface HeaderProps extends React.HTMLAttributes<HTMLDivElement>{
+    children: React.ReactNode
+    className?: string
+}
+
+export default function Header( {children, className}: HeaderProps ){
+    return (
+        <div className="">
+            {children}
+        </div>
+    )
+}
+
+Header.Subtitle = function Subtitle({children, className}: HeaderProps){
+    return (
+        <div>
+            {children}
+        </div>
+    )
+}
