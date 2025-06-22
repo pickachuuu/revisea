834f7ebe3adace3762022e930906babc75f97053
Derek <dberb789@gmail.com>
2025-06-22 17:28:03 +0800

    feat: import and add dataCard and RecentCard

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -1,11 +1,13 @@
 import Button from "@/component/ui/Button";
 import { File01Icon, PencilEdit02Icon } from "hugeicons-react";
+import DataCard from "@/component/features/DataCard";
+import RecentCard from "@/component/features/RecentCard";
 
 export default function DashboardContentLayout({ children }:Readonly<{
   children: React.ReactNode;
 }>){
     return (
-        <div className="">
+        <div className="w-ful">
             <div className="flex gap-3">
             <Button Icon={PencilEdit02Icon} variant={'secondary'} size={'sm'} className="font-serifs font-semibold text-lg bg-foreground">
                 New Note
@@ -16,6 +18,21 @@ export default function DashboardContentLayout({ children }:Readonly<{
             </Button>
             </div>
 
+            <div className="grid grid-cols-4 gap-3 my-7 w-full">
+                <DataCard/>
+                <DataCard/>
+                <DataCard/>
+                <DataCard/>
+            </div>
+
+            <div className="grid grid-cols-2 gap-4 w-full">
+                <div>
+                    <RecentCard/>
+                </div>
+                <div>
+                    <RecentCard/>
+                </div>
+            </div>
             {children}
         </div>
     )
